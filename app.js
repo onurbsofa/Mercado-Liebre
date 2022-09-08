b68f8b6d684b7956cddfc8a50a7a7d74b14fcf8f
BoFo <brunofazio466@gmail.com>
2022-09-08 15:23:27 -0300

    entrega eduflow

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const app = express() ;
 const publicPath = path.resolve(__dirname, './public');
 app.use( express.static(publicPath) ); 
 
-app.listen(3030,function() {
-    console.log('Corriendo');
-});
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+})
 
 app.get('/home', (req,res) => {
     res.sendFile(path.join(__dirname,'./views/home.html'));
