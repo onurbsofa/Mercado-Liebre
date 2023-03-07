a195deda1cd293d1d410bf4b7403f9706f97041c
Bruno Fazio <brunofazio466@gmail.com>
2023-03-07 12:22:29 -0300

    proyecto base

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const path = require('path');
 
-const app = express() ;
+const express = require('express');
+const app = express();
+const path = require('path');
 
 const publicPath = path.resolve(__dirname, './public');
 app.use( express.static(publicPath) ); 
@@ -13,3 +13,11 @@ app.listen(process.env.PORT || 3000, function() {
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,'./views/home.html'));
 });
+
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname,'./views/register.html'));
+});
+
+app.get('/login',(req,res) => {
+    res.sendFile(path.join(__dirname,'/views/login.html'))
+})
